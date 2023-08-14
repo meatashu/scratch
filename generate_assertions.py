fbca6a0d7e5031dc8335fc28274bee225a017109
Ashutosh Pandey <meatashu@gmail.com>
2023-08-13 23:52:19 -0400

    Create generate_assertions.py

---
+++ generate_assertions.py
@@ -0,0 +1,66 @@
+import os
+import re
+
+def read_sql_script(filename):
+  """
+  Read a SQL script from a file and return the contents as a string.
+
+  Args:
+    filename: The path to the SQL script file.
+
+  Returns:
+    The contents of the SQL script file as a string.
+  """
+
+  with open(filename, "r") as f:
+    script = f.read()
+  return script
+
+def generate_assertions(script):
+  """
+  Generate assertions from a SQL script.
+
+  Args:
+    script: The contents of a SQL script as a string.
+
+  Returns:
+    A list of assertions.
+  """
+
+  assertions = []
+  for line in script.splitlines():
+    # Match HQL queries
+    match = re.match("^[^#]*(select|from|where|group by|order by|limit)", line)
+    if match:
+      assertions.append(assert_hql_correct(line))
+
+  return assertions
+
+def assert_hql_correct(hql):
+  """
+  Assert that an HQL query is correct.
+
+  Args:
+    hql: The HQL query to assert.
+
+  Returns:
+    An assertion that will fail if the HQL query is not correct.
+  """
+
+  def check_query():
+    """
+    A function that checks if the HQL query is correct.
+    """
+    result = hive.query(hql)
+    if not result:
+      raise Exception("HQL query '{}' failed.".format(hql))
+
+  return check_query
+
+if __name__ == "__main__":
+  script = read_sql_script("test.sql")
+  assertions = generate_assertions(script)
+  for assertion in assertions:
+    assertion()
+
+  print("All assertions passed!")
