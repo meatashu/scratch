a39634906505dfec1d8077a8ec80a8f79397f827
Ashutosh Pandey <meatashu@gmail.com>
2023-08-14 00:00:34 -0400

    Update verify_release_version.java

--- verify_release_version.java
+++ verify_release_version.java
@@ -1,6 +1,14 @@
 import java.io.File;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.jfrog.artifactory.client.Artifactory;
+import org.jfrog.artifactory.client.ArtifactoryClientBuilder;
+import org.jfrog.artifactory.client.ArtifactoryVersion;
+import org.jfrog.artifactory.client.impl.ArtifactoryImpl;
+import org.jfrog.artifactory.client.model.RepositoryItem;
 
 public class JarValidator {
 
@@ -12,7 +20,7 @@ public class JarValidator {
         // Get the list of dependencies from the manifest.
         String[] dependencies = manifest.getMainAttributes().getValue("Dependencies").split(",");
 
-        // Check if all dependencies are from a RELEASE version.
+         // Check if all dependencies are from a RELEASE version.
         for (String dependency : dependencies) {
             String version = dependency.split(":")[1];
             if (version.endsWith("-SNAPSHOT")) {
@@ -20,6 +28,27 @@ public class JarValidator {
             }
         }
 
-        System.out.println("The jar contains all dependencies from a RELEASE version and no snapshot versions.");
+        // Create an Artifactory client.
+        Artifactory artifactory = new ArtifactoryImpl("https://artifactory.example.com", "admin", "password", ArtifactoryVersion.LATEST);
+
+        // Get the latest versions of the dependencies from Artifactory.
+        List<RepositoryItem> latestVersions = dependencies.stream()
+            .map(dependency -> artifactory.getRepository("libs-release").getItem(dependency))
+            .collect(Collectors.toList());
+
+        // Check if the jar file is using the latest versions of the dependencies.
+        for (int i = 0; i < dependencies.length; i++) {
+            String dependency = dependencies[i];
+            RepositoryItem latestVersion = latestVersions.get(i);
+
+            String version = dependency.split(":")[1];
+            String latestVersionString = latestVersion.getVersion();
+
+            if (!version.equals(latestVersionString)) {
+                throw new Exception("The jar contains an outdated dependency: " + dependency + " (latest version: " + latestVersionString + ")");
+            }
+        }
+
+        System.out.println("The jar contains all dependencies from the latest available version.");
     }
 }
